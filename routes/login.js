0157e18deeebf92a3566be6a9a6d003d4041ea00
VitorCioletti <ciolettiv@gmail.com>
2018-09-13 12:20:22 -0300

    Deletado código antigo da rota de login

--- routes/login.js
+++ routes/login.js
@@ -7,21 +7,6 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-    var erros = ValidaPreenchimentoObrigatorio(req)
-    if (!erros){
-        var usuario = { email: req.body.email, senha: req.body.senha }
-        
-        console.log(usuario)
-
-        arangodb.ObtemUsuarios(function(objeto) {
-            console.log(objeto)
-        });
-    }
-    else{
-        res.render('login', {
-            errors: erros
-        })
-    }
 })
 
 module.exports = router
