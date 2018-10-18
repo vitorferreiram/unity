30bed47051fa6f70f175bf9fa81237a2698819a4
Vitor Cioletti <vitorfmarques@live.com>
2018-10-18 20:47:32 -0300

    Adicionado classe de ocorrencia

---
+++ models/entidades/Ocorrencia.js
@@ -0,0 +1,11 @@
+function Ocorrencia(ocorrencia) {
+    this.Id = ocorrencia.Id,
+    this.Descricao = ocorrencia.Descricao,
+    this.Acao = ocorrencia.Descricao,
+    this.Pensamento = ocorrencia.Descricao,
+    this.fk_Humor = ocorrencia.Humor,
+    this.fk_Sentimento = ocorrencia.Sentimento,
+    this.Escala = ocorrencia.Escala,
+    this.Criacao = ocorrencia.Criacao,
+    this.fk_Criador = ocorrencia.fk_Criador
+}
