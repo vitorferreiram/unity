31293c5446bad4e196749f5e3b94fc16307d3835
Vitor Cioletti <ciolettiv@gmail.com>
2018-10-18 19:50:24 -0300

    Adicionada entidade usuario

--- models/entidades/Terapeuta.js
+++
@@ -1,8 +0,0 @@
-function Terapeuta(dadosTerapeuta) {
-    this.Nome = dadosTerapeuta.Nome,
-    this.Email = dadosTerapeuta.Email,
-    this.Senha = dadosTerapeuta.Senha,
-    this.Registro = '',
-    this.SalSenha = '',
-    this.Tipo = 'T'
-}
---
+++ models/entidades/Usuario.js
@@ -0,0 +1,7 @@
+function Usuario(dadosUsuario) {
+    this.Nome = dadosUsuario.Nome,
+    this.Email = dadosUsuario.Email,
+    this.Senha = dadosUsuario.Senha,
+    this.SalSenha = '',
+    this.Tipo = dadosUsuario.Tipo
+}
