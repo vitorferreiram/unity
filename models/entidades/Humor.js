d4207832f1ecd4e010159784f6b5b55a0186cc4e
Vitor Cioletti <vitorfmarques@live.com>
2018-10-18 20:45:22 -0300

    Removida propriedade de escala de sentimento

--- models/entidades/Humor.js
+++ models/entidades/Humor.js
@@ -1,7 +1,6 @@
 function Humor(dadosHumor) {
     this.Id = dadosHumor.Id,
     this.Descricao = dadosHumor.Descricao,
-    this.Escala = dadosHumor.Escala,
     this.Criacao = dadosHumor.Criacao,
     this.fk_Criador = dadosHumor.fk_Criador
 }
